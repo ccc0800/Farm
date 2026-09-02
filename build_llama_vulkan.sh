f199baf0603bcbb5d03342f7eaaeed1d856a2e70
ccc0800 <eric.chang0800@gmail.com>
2026-09-02 12:22:09 +0800

    Update build_llama_vulkan.sh

--- build_llama_vulkan.sh
+++ build_llama_vulkan.sh
@@ -13,6 +13,10 @@ podman run --rm -it \
       cmake git build-essential python3 python3-pip \
       libvulkan-dev vulkan-tools glslc glslang-tools spirv-tools \
       mesa-vulkan-drivers &&
+    # Ubuntu 的 spirv-headers deb 套件缺 CMake config,直接編源碼裝一份
+    git clone --depth 1 https://github.com/KhronosGroup/SPIRV-Headers /tmp/spirv-headers &&
+    cmake -S /tmp/spirv-headers -B /tmp/spirv-headers/build -DCMAKE_INSTALL_PREFIX=/usr/local &&
+    cmake --build /tmp/spirv-headers/build --target install &&
     git clone https://github.com/ggml-org/llama.cpp /tmp/llama &&
     cd /tmp/llama &&
     cmake -B build \
